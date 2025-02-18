555946a169c49a35a412c418aa6040b53011fa28
Isasiti <141294077+Isasiti@users.noreply.github.com>
2025-02-18 13:32:05 -0500

    Clase notebook integrada

--- app/notebook.py
+++ app/notebook.py
@@ -22,3 +22,15 @@ class Note:
     def __str__(self):
         # Formato de la cadena que se retorna
         return f"Date: {self.creation_date}\n{self.title}: {self.text}"
+class Notebook:
+    def __init__(self):
+        self.notes = []
+    def add_note(self, title: str, text: str, importance: str) -> int:
+        # Generar el código único para la nueva nota
+        new_code = len(self.notes) + 1  # Genera un código único mayor que 0
+        
+        # Crear la nueva nota con los parámetros recibidos
+        new_note = Note(title, text, importance, new_code)
+        
+        # Agregar la nueva nota a la lista de notas
+        self.notes.append(new_note)
